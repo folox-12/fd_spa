c2a0b0e0bfa60840339d430bd9d660ab79bc5727
woe10116 <woe10116@gmail.com>
2022-03-30 15:34:20 +0300

    add filter submenu

--- frontend/static/js/views/LIst.js
+++ frontend/static/js/views/LIst.js
@@ -31,18 +31,80 @@ export default class extends AbstractView {
 			<div class="header-page-search__input">
 			<img src = "static/img/ico/search.svg" alt="" class = "search-icon">
 			<input type="text" placeholder = "Какой-то поиск" id = "input-main" >
-			<button class="clear-input" id = "clear-input">
-			<img src="static/img/ico/close.svg"  alt="" class = "filter-icon" >
+			<button class="clear-input" id = "clear-input" onclick = "clearInput()">
+			<img src="static/img/ico/close.svg"  alt="" class = "filter-icon2" >
 			</button>
 			</div>
 			<div class="header-page-search__buttons">
 			<button  class = "filter"  id ="button_filter">
 			<img src="static/img/ico/filter.svg"  alt="" class = "filter-icon" >
 		</button>
-		<button  class = "filter"  id ="button_filter">
-		<img src="static/img/ico/settings.svg"  alt="" class = "filter-icon" >
+		<button  class = "filter1"  id ="button_filter1">
+		<img src="static/img/ico/settings.svg"  alt="" class = "filter-icon1" >
 	</button>
 			</div>
+		</div>
+	</div>
+
+	<div class = "main-block">
+
+	<div class="filter-block">
+<div id="myDropdown" class="dropdown-content">
+	
+	<div class = "spoiler-main" id = "spoiler-main">
+		<div class="title_dropdown">
+			<div class="name_title">
+				<h3>Фильтр</h3>
+			</div>
+			<div class="close-dropdown">
+				<button>
+					<img src="static/img/ico/close.svg"  alt="" class = "close-svg" >
+				</button>
+			</div>
+		</div>
+		<div class = "spoiler__item">
+			<div class = "spoiler__title">
+			Город 
+			</div>
+			<div class = "spoiler__content">
+				<div class="input-spoiler-content">
+					<input type="text" placeholder = "Какой-то поиск" id = "input-filter" class = "input-filter">
+					<button class="clear-input" id = "clear-input-filter" onclick = "clearInput1()">
+						<span class="material-icons">✖</span>
+					</button>
+				</input>
+				</div>
+			</div>
+		</div>
+		<hr>
+		<div class = "spoiler__item">
+		<div class = "spoiler__title">
+		Дронопорт
+		</div>
+		<div class = "spoiler__content">
+		<div class = "spoiler__content-checkbox-filter">
+		<div class = "checkbox-filter__item">
+		<input type = "checkbox" class = "checkbox__input" id = "checkbox1">
+		<label class = "chechbox__label" for = "checkbox1">Ни одного</label>
+		</div>
+		<div class = "checkbox-filter__item">
+		<input type = "checkbox" class = "checkbox__input" id = "checkbox2">
+		<label class = "chechbox__label" for = "checkbox2">Несколько </label>
+		</div>
+		</div>
+		</div>
+		</div>
+		<hr>
+		<div class = "spoiler__item">
+		<div class = "spoiler__title">
+		Обслуживаемые дроны
+		</div>
+		<div class = "spoiler__content">
+		Какой-то фильтр
+		</div>
+		</div>
+		<hr>
+		</div>
 		</div>
 	</div>
 		<div class="table">
@@ -80,6 +142,8 @@ export default class extends AbstractView {
 		</div>
 	</div>
 	</div>
+
+
 		`;
 	}
 }
